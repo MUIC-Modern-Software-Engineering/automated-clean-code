184c085a11de972605b87a31b2643570c3e021b0
Piti Ongmongkolkul <piti118@gmail.com>
2023-01-27 04:41:34 +0700

    worst code ever

--- src/automated_clean_code/__init__.py
+++ src/automated_clean_code/__init__.py
@@ -13,9 +13,12 @@ def add_numbers(x: int, y: int) -> int:
 
 def super_for_loop(n: int) -> int:
     x = 0
-    for _ in range(n):
+    try:
         for _ in range(n):
             for _ in range(n):
                 for _ in range(n):
                     for _ in range(n):
-                        x += 1
+                        for _ in range(n):
+                            x += 1
+    except:
+        raise ValueError()
