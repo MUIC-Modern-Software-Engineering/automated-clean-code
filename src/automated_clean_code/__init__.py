2e22a351b0240551979781aa8d5ee63e0116a14c
justyn lantz <justyn@DESKTOP-7JDRPAI.localdomain>
2023-01-27 15:32:28 +0700

    add subtract numbers method

--- src/automated_clean_code/__init__.py
+++ src/automated_clean_code/__init__.py
@@ -10,3 +10,15 @@ def add_numbers(x: int, y: int) -> int:
       (int). The sum of the two numbers.
     """
     return x + y
+
+
+def subtract_numbers(x: int, y: int) -> int:
+    """Subtract two numbers.
+
+    Args:
+        x (int): a number
+        y (int): a number
+    Returns:
+        (int). The sum of the two numbers.
+    """
+    return x - y
