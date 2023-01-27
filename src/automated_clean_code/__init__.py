61a8cbaded98af268c988bae493420905d55f6ff
karank85 <kkjas85@gmail.com>
2023-01-27 15:34:13 +0700

    add subtract

--- src/automated_clean_code/__init__.py
+++ src/automated_clean_code/__init__.py
@@ -2,10 +2,23 @@ def add_numbers(a: int, b: int) -> int:
     """Add two numbers together.
 
     Args:a
-      x (int): a number
-      y (int): a number
+      a (int): a number
+      b (int): a number
 
     Returns:
       (int). The sum of the two numbers.
     """
     return a + b
+
+
+def subtract_numbers(x: int, y: int) -> int:
+    """Subtract two numbers together.
+
+    Args:a
+      x (int): a number
+      y (int): a number
+
+    Returns:
+      (int). The difference of the two numbers.
+    """
+    return x - y
