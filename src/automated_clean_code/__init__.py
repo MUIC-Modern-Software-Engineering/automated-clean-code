535b61c753be8efd9a58b5e65f6147b9787fa5d2
Piti Ongmongkolkul <piti118@gmail.com>
2023-01-27 15:31:27 +0700

    update ignore pre-commit

--- src/automated_clean_code/__init__.py
+++ src/automated_clean_code/__init__.py
@@ -1,3 +1,9 @@
+import math
+import os
+
+import pkg_resources
+
+
 def add_numbers(x: int, y: int) -> int:
     """Add two numbers together.
 
@@ -9,3 +15,7 @@ def add_numbers(x: int, y: int) -> int:
       (int). The sum of the two numbers.
     """
     return x + y
+
+
+def subtract_numbers(x: int, y: int) -> int:
+    return x -                          y   # pragma: no cover
